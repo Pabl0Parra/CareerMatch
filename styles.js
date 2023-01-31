7d056440125c48c3c88eaf8e188b916c109c87ce
Pabl0Parra <pabsblue16@gmail.com>
2023-01-31 23:29:48 +0100

    add basic styles

---
+++ styles.js
@@ -0,0 +1,38 @@
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listingContainer: {
+    flexDirection: "row",
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 20,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  company: {
+    color: "#999",
+    fontSize: 14,
+  },
+  selectedContainer: {
+    padding: 20,
+  },
+  description: {
+    fontSize: 14,
+  },
+});
